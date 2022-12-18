30a6c79adad5a54fe359ecf13b6aa0160891d2f7
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-18 10:40:27 -0600

    Linq tests and code

--- TopicExampleCode/LinqEfficiency.cs
+++ TopicExampleCode/LinqEfficiency.cs
@@ -1,6 +1,75 @@
 ﻿namespace TopicExampleCode;
 
-public class LinqEfficiency
+public static class LinqEfficiency
 {
-    
+    // Make sure you are giving a capacity for lists that need to take a lot of elements.
+    // This will make the allocation much more optimized.
+    public static List<string> BigAllocationForEach()
+    {
+        var list = new List<string>(200_000_000);
+        foreach (var _ in Enumerable.Range(0, 200_000_000))
+        {
+            list.Add("Really big string.");
+        }
+
+        return list;
+    }
+
+    // This is slower than using Linq to generate the list.
+    public static List<string> BigAllocationForEachWithoutSize()
+    {
+        var list = new List<string>();
+        foreach (var _ in Enumerable.Range(0, 200_000_000))
+        {
+            list.Add("Really big string.");
+        }
+
+        return list;
+    }
+
+    // This is ideal, but only for situations where you can get the capacity needed for your array.
+    // For example, if you are querying a DB and can page the results you could easily use an array.
+    public static string[] BigAllocationFor()
+    {
+        var list = new string[200_000_000];
+        for (var i = 0; i < list.Length; i += 1)
+        {
+            list[i] = "Really big string.";
+        }
+
+        return list;
+    }
+
+    // This is inefficient, the benefit here is that the work is lazy but wouldn't really benefit you
+    // if you need to do an operation on every element.
+    public static List<string> BigAllocationLinq()
+    {
+        var list = new List<string>(200_000_000);
+        list.AddRange(Enumerable.Range(0, 200_000_000).Select(_ => "Really big string."));
+
+        return list;
+    }
+
+    // This is also very inefficient, see notes for above.
+    public static IEnumerable<string> BigAllocationGenerator()
+    {
+        foreach (var _ in Enumerable.Range(0, 200_000_000))
+        {
+            yield return "Really big string";
+        }
+    }
+
+    public static IDictionary<int, string> GenerateDictionaryManual()
+    {
+        var dictionary = new Dictionary<int, string>();
+        foreach (var i in Enumerable.Range(0, 200_000_000))
+        {
+            dictionary.Add(i, "Test");
+        }
+
+        return dictionary.ToImmutableDictionary();
+    }
+
+    public static IDictionary<int, string> GenerateDictionaryLinq() =>
+        Enumerable.Range(0, 200_000_000).ToDictionary(i => i, i => "Test").ToImmutableDictionary();
 }
--- TopicExamplesTests/LinqEfficiencyTests.cs
+++ TopicExamplesTests/LinqEfficiencyTests.cs
@@ -1,6 +1,58 @@
 ﻿namespace Tests;
 
-public class LinqEfficiencyTests
+public static class LinqEfficiencyTests
 {
-    
+    // Fastest, it's going to take around 11 seconds. This is great for when you know how many elements you need exactly.
+    [Fact]
+    public static void BigAllocationForTest()
+    {
+        var _ = LinqEfficiency.BigAllocationFor();
+    }
+
+    // Takes around 21 seconds. Faster than Linq but still not great.
+    [Fact]
+    public static void BigAllocationForEachTest()
+    {
+        var _ = LinqEfficiency.BigAllocationForEach();
+    }
+
+    // Surprisingly faster than using a foreach with no initial size. If you don't know how many elements you will need
+    // and can't get close it may be okay to use a Linq select.
+    // Your mileage may vary, and will need profiling.
+    // Takes around 27 seconds.
+    [Fact]
+    public static void BigAllocationLinqTest()
+    {
+        var _ = LinqEfficiency.BigAllocationLinq();
+    }
+
+    // 55 seconds, it's recommended to only use this when you are going to be chaining operations together
+    // that way you don't allocate and process items you don't need.
+    // In other words, the less you force enumeration the better.
+    [Fact]
+    public static void GeneratorTest()
+    {
+        // This won't actually work until we tell it to work with the ToList call.
+        // Enumerable is lazy and won't work until you are explicit about needing it.
+        var _ = LinqEfficiency.BigAllocationGenerator().ToList();
+    }
+
+    // Slowest, takes 61 seconds to do the process. 
+    [Fact]
+    public static void BigAllocationForEachWithoutSizeTest()
+    {
+        var _ = LinqEfficiency.BigAllocationForEachWithoutSize();
+    }
+
+    [Fact]
+    public static void ImmutableDictionaryManualTest()
+    {
+        var _ = LinqEfficiency.GenerateDictionaryManual();
+    }
+
+    [Fact]
+    public static void ImmutableDictionaryLinqTest()
+    {
+        var _ = LinqEfficiency.GenerateDictionaryLinq();
+    }
 }
