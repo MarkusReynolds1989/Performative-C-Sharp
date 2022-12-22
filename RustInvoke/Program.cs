2deb4370cca6936136646738e8c78beb364dc752
Markus Reynolds <markusreynolds1989@gmail.com>
2022-12-22 12:02:03 -0600

    Rust Invoke Examples

---
+++ RustInvoke/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RustInvoke;
+
+internal static unsafe class Program
+{
+    private ref struct Person
+    {
+        public byte* Name;
+        public int Age;
+
+        public Person(byte* name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    [DllImport(@"C:\Users\marku\Code\C#\Performative-C-Sharp\low_level_algo\target\debug\low_level_algo.dll")]
+    private static extern Person create_person(nint nameBytes, uint count, int age);
+
+    private static void Main()
+    {
+        const int nameSize = 3;
+        var namePointer = stackalloc byte[nameSize];
+        var name = new Span<byte>(namePointer, nameSize)
+        {
+            [0] = (byte) 't',
+            [1] = (byte) 'o',
+            [2] = (byte) 'm'
+        };
+
+        var tom = create_person((nint) namePointer, 3, 32);
+        Console.WriteLine($"Name: {Encoding.UTF8.GetString(new Span<byte>(tom.Name, 3))}, Age: {tom.Age}");
+    }
+}
---
+++ low_level_algo/src/lib.rs
@@ -0,0 +1,40 @@
+use std::str;
+use std::{mem, slice};
+
+#[no_mangle]
+pub unsafe extern "C" fn reverse_array(input: *const i32, count: usize) -> *const i32 {
+    let items = slice::from_raw_parts(input, count);
+    let vector = items
+        .iter()
+        .rev()
+        .map(|i| -> i32 { *i })
+        .collect::<Vec<i32>>();
+
+    // Use this when I want to transfer the ownership to outside of rust.
+    // Now the caller will have to handle the memory.
+    let result = vector.as_ptr();
+    mem::forget(vector);
+
+    result
+}
+
+pub struct Person {
+    name: *const u8,
+    age: i32,
+}
+
+impl Person {
+    #[no_mangle]
+    pub unsafe extern "C" fn create_person(
+        name_bytes: *const u8,
+        count: usize,
+        age: i32,
+    ) -> Person {
+        let name = str::from_utf8(slice::from_raw_parts(name_bytes, count)).unwrap().to_uppercase();
+
+        Person {
+            name: name.as_bytes().as_ptr(),
+            age: age + 1,
+        }
+    }
+}
