1573e64355396913f9aabdfc96acad8616ad2da8
Markus Reynolds <markusreynolds1989@gmail.com>
2023-01-22 09:15:03 -0600

    Added parallel linq as well

--- TopicExampleCode/Parallelization.cs
+++ TopicExampleCode/Parallelization.cs
@@ -2,9 +2,9 @@
 
 public static class Parallelization
 {
-    public static IEnumerable<int> StringsToIntegers(IEnumerable<string> input)
+    public static IEnumerable<int> StringsToIntegers(int initialCapacity, IEnumerable<string> input)
     {
-        var output = new List<int>();
+        var output = new List<int>(initialCapacity);
         foreach (var line in input)
         {
             output.Add(int.Parse(line));
@@ -13,10 +13,13 @@ public static class Parallelization
         return output;
     }
 
-    public static IEnumerable<int> StringsToIntegersParallel(IEnumerable<string> input)
+    public static IEnumerable<int> StringsToIntegersParallel(int initialCapacity, IEnumerable<string> input)
     {
-        var output = new List<int>();
+        var output = new List<int>(initialCapacity);
         Parallel.ForEach(input, line => { output.Add(int.Parse(line)); });
         return output;
     }
+
+    public static IEnumerable<int> StringsToIntegersParallelLinq(IEnumerable<string> input) =>
+        input.AsParallel().Select(int.Parse);
 }
--- TopicExamplesTests/ParallelizationTests.cs
+++ TopicExamplesTests/ParallelizationTests.cs
@@ -3,6 +3,7 @@
 public class ParallelizationTests
 {
     private readonly string[] _shortFile = File.ReadAllLines("strings_short.txt");
+
     // You will have to make your own "strings_long.txt" file or follow along.
     // Mine was 100_000_000 lines. It's just a file with the numbers 1 to 100_000_00 written on one line each.
     private readonly string[] _longFile = File.ReadAllLines("strings_long.txt");
@@ -13,7 +14,7 @@ public class ParallelizationTests
         // This test will run faster because Parallelization takes time to setup.
         // You shouldn't use parallelization unless you have a large list of items you can handle
         // simultaneously.
-        var results = Parallelization.StringsToIntegers(_shortFile);
+        var results = Parallelization.StringsToIntegers(_shortFile.Length, _shortFile).ToList();
     }
 
     [Fact]
@@ -21,21 +22,30 @@ public class ParallelizationTests
     {
         // This will run slower because it has set up rather then just starting to translate right away.
         // In parallel, turn all strings into integers.
-        var results = Parallelization.StringsToIntegersParallel(_shortFile);
+        var results = Parallelization.StringsToIntegersParallel(_shortFile.Length, _shortFile).ToList();
     }
 
     [Fact]
+    //[Fact(Skip="Enable manually, will take a long time.")]
     public void AllStringsAreNumbersLong()
     {
         // Here's where we will see the speedup, the single threaded solution 
         // Will use one thread to go through the file one at a time to convert.
-        var results = Parallelization.StringsToIntegers(_longFile);
+        var results = Parallelization.StringsToIntegers(_longFile.Length, _longFile).ToList();
     }
 
+    //[Fact(Skip="Enable manually, will take a long time.")]
     [Fact]
     public void AllStringsAreNumbersParallelLong()
     {
         // The parallel implementation will run multiple threads to do the work on the strings.
-        var results = Parallelization.StringsToIntegersParallel(_longFile);
+        var results = Parallelization.StringsToIntegersParallel(_longFile.Length, _longFile).ToList();
+    }
+
+    //[Fact(Skip="Enable manually, will take a long time.")]
+    [Fact]
+    public void AllStringsAreNumbersParallelLinq()
+    {
+        var results = Parallelization.StringsToIntegersParallelLinq(_longFile).ToList();
     }
 }
