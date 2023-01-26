5a6f125246b7344070f0c2d317652fb566683bc4
Markus Reynolds <markusreynolds1989@gmail.com>
2023-01-25 19:02:22 -0600

    Additions to time complexity.

--- TopicExampleCode/AlgorithmicTimeSpaceComplexity.cs
+++ TopicExampleCode/AlgorithmicTimeSpaceComplexity.cs
@@ -3,7 +3,7 @@
 public class AlgorithmicTimeSpaceComplexity
 {
     // O(1) <- Always the same amount, no variance.
-    public static void ConstantComplexity()
+    public static int ConstantComplexity()
     {
         var array = new[] {1, 2, 3, 4};
         // Getting an item from an array by index is a constant time operation.
@@ -13,7 +13,7 @@ public class AlgorithmicTimeSpaceComplexity
         // Usually, a collection implementation has documentation about what kind of complexity
         // you can expect for different operations.
 
-        Console.WriteLine(array[1]);
+        return array[1];
     }
 
     // O(n log n) <- where n is the amount of items.
@@ -27,57 +27,74 @@ public class AlgorithmicTimeSpaceComplexity
     }
 
     // O(n) <- where n is the amount of items.
-    public static void LinearComplexity()
+    public static List<int> LinearComplexity()
     {
         // Each item will be touched one at a time until the collection is done.
         // This is linear complexity, the time it takes to do the process scales with 
         // the amount of elements there are in the collection.
         var array = new[] {1, 2, 3, 4};
+        var newList = new List<int>();
         foreach (var item in array)
         {
-            Console.WriteLine(item);
+            newList.Add(item + 1);
         }
+
+        return newList;
     }
 
     // O(n^2) <- Where n is the amount of items.
-    public static void QuadraticComplexity()
+    public static List<List<int>> QuadraticComplexity()
     {
         // A typical example of quadratic time is when you have a nested for loop.
         // This increases the time complexity because you have to cycle through the top collection and then each sub collection.
-        var list = new List<List<int>> { };
-        list.Add(new List<int> {1, 2, 3, 4});
-        list.Add(new List<int> {1, 2, 3, 4});
+        var newList = new List<List<int>>();
+        var list = new List<List<int>>
+        {
+            new() {1, 2, 3, 4},
+            new() {1, 2, 3, 4}
+        };
 
+        var items = new List<int>();
         foreach (var item in list)
         {
             foreach (var subItem in item)
             {
-                Console.WriteLine(subItem);
+                items.Add(subItem + 1);
             }
+
+            newList.Add(items);
+            items.Clear();
         }
+
+        newList.Clear();
 
         // Any time you see a quadratic algorithm and you've profiled it to verify it's slow in the context
         // of your program, you should refactor it so that it's flat.
         // Thus, you can reduce the time complexity by handling the complexity it yourself.
-        // TODO: Test for double checking.
         // This algorithm now has linear complexity.
         var innerIndex = 0;
-        for (var outerIndex = 0; outerIndex < list.Count; outerIndex += 1)
+        var outerIndex = 0;
+        while (outerIndex < list.Count)
         {
             if (innerIndex < list[outerIndex].Count)
             {
-                Console.WriteLine(list[outerIndex][innerIndex]);
+                items.Add(list[outerIndex][innerIndex] + 1);
                 innerIndex += 1;
             }
             else
             {
                 innerIndex = 0;
+                outerIndex += 1;
+                newList.Add(items);
+                items = new List<int>();
             }
         }
+
+        return newList;
     }
 
     // O(2^n) <- Where n is the amount of items.
-    public static void ExponentialComplexity()
+    public static List<List<List<int>>> ExponentialComplexity()
     {
         // This is often seen in situations where you have 3 + nested loops.
         // If you have a collection of collections and you need to gather some sort of permutation data
@@ -86,5 +103,34 @@ public class AlgorithmicTimeSpaceComplexity
         // worth a refactor.
         // There are some problems that REQUIRE this approach, however, such as the traveling salesman problem using
         // dynamic programming.
+        var deepList = new List<List<List<int>>>
+        {
+            new() {new() {1, 2, 3, 4}, new() {1, 2, 3, 4}, new() {1, 2, 3, 4}},
+            new() {new() {1, 2, 3, 4}, new() {1, 2, 3, 4}, new() {1, 2, 3, 4}}
+        };
+
+        var newDeepList = new List<List<List<int>>>();
+
+        foreach (var top in deepList)
+        {
+            var topList = new List<List<int>>();
+
+            foreach (var middle in top)
+            {
+                var middleList = new List<int>();
+                foreach (var bottom in middle)
+                {
+                    // Here we are brute forcing our way to the bottom item.
+                    // Usually you see this in situations where you have a collection of classes with deep fields inside.
+                    middleList.Add(bottom + 1);
+                }
+
+                topList.Add(middleList);
+            }
+
+            newDeepList.Add(topList);
+        }
+
+        return newDeepList;
     }
 }
--- TopicExamplesTests/AlgorithmicTimeSpaceComplexityTests.cs
+++ TopicExamplesTests/AlgorithmicTimeSpaceComplexityTests.cs
@@ -2,5 +2,29 @@
 
 public class AlgorithmicTimeSpaceComplexityTests
 {
-    
+    [Fact]
+    public void QuadraticTimeComplexityTest()
+    {
+        var results = AlgorithmicTimeSpaceComplexity.QuadraticComplexity();
+        var expected = new List<List<int>>
+        {
+            new() {2, 3, 4, 5},
+            new() {2, 3, 4, 5}
+        };
+
+        Assert.Equal(expected, results);
+    }
+
+    [Fact]
+    public void ExponentialTimeComplexityTest()
+    {
+        var results = AlgorithmicTimeSpaceComplexity.ExponentialComplexity();
+        var expected = new List<List<List<int>>>
+        {
+            new() {new() {2, 3, 4, 5}, new() {2, 3, 4, 5}, new() {2, 3, 4, 5}},
+            new() {new() {2, 3, 4, 5}, new() {2, 3, 4, 5}, new() {2, 3, 4, 5}}
+        };
+
+        Assert.Equal(expected, results);
+    }
 }
